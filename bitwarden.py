0bc24b8d0f83c6b0ac2eb98d6cef246f3f50abb7
BuildTools <elias123tre>
2021-04-19 02:08:10 +0200

    first commit

---
+++ bitwarden.py
@@ -0,0 +1,59 @@
+"""Find duplicates from bitwarden export and present them in as a html website"""
+# %% Imports
+import json
+import os
+import re
+import sys
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+from dupfinder import dups_uris
+
+# %% Select a file
+Tk().withdraw()  # Hide empty window
+filename = askopenfilename(**{
+    "title": "Select your Bitwarden export file (.json extension)",
+    "filetypes": [("Bitwarden export", "*.json")],
+    "initialdir": os.getcwd()
+})
+if not filename:
+    print("Error: no file selected.")
+    sys.exit(1)
+
+# %% Get items and folders from file
+with open(filename, encoding="utf-8") as f:
+    data: dict[list[dict]] = json.load(f)
+    folders = data.get("folders", [])
+    items = data.get("items", [])
+
+# %% Expand login descendats (make username & password top level)
+for row in items:
+    for key, val in row.get("login", {}).items():
+        row[key] = val
+
+# %% Filter for accounts
+items = [i for i in items if i.get("type") == 1]
+
+
+# %% Define uri cleaning function
+def domains(entry):
+    """Get domains from login"""
+    def transform(url):
+        match = re.match(r"(\w+:\/\/)?([\w.]+)(\/?.*)", url)
+        if match:
+            return ".".join(match.group(2).removeprefix("www.").split(".")[-2:])
+        return ""
+    return set(filter(None, (transform(uri.get("uri")) for uri in entry.get("uris", []))))
+
+
+# %% Write object to file
+duplicates = dups_uris(items, func=domains)
+
+with open("logins.js", "w", encoding='utf-8') as f:
+    data = json.dumps(duplicates, sort_keys=True)
+    f.write("const logins = JSON.parse('")
+    f.write(re.escape(data))
+    f.write("')")
+
+# %% Open in visual html file
+os.startfile("index.html")
---
+++ dupfinder.py
@@ -0,0 +1,51 @@
+"""Different algorithms for finding duplicates"""
+from itertools import groupby
+
+
+def itemrepr(entry, keys=("name", "username", "password", "fields")):
+    """Simple representation of login item"""
+    rep = {k: v for k, v in entry.items() if k in keys}
+    return rep
+
+
+def dups_uris(items, func):
+    """Duplicates by set and find"""
+    uris = set()
+    for item in items:
+        item["domains"] = []
+        for uri in func(item):
+            item["domains"].append(uri)
+            uris.add(uri)
+
+    duplicates = {}
+    for uri in sorted(list(uris)):
+        filtered = [e for e in items if uri in e.get("domains", [])]
+        if len(filtered) > 1:
+            duplicates[uri] = filtered
+    return duplicates
+
+
+def dups_sort(items, func):
+    """Duplicates by sorting and groupby
+    ! DEPRECATED: NOT WORKING
+    """
+    items.sort(key=func)
+    duplicates = [(k, [itemrepr(e) for e in g])
+                  for k, g in groupby(items, key=func)]
+    duplicates = [(dom, ents) for dom, ents in duplicates if len(ents) > 1]
+    return duplicates
+
+
+def dups_seen(items, func):
+    """Duplicates by seen
+    ! DEPRECATED: NOT WORKING
+    """
+    seen = set()
+    for item in items:
+        uris = func(item)
+        if any(uri in seen for uri in uris):
+            print("Duplicate:")
+            yield {k: v for k, v in item.items() if k in ("name", "username", "password", "fields")}
+            print()
+        for uri in uris:
+            seen.add(uri)
