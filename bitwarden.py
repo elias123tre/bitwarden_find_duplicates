e01b6b028952123be34e96da3bdc8f9917dde290
Elias Floreteng <18127101+elias123tre@users.noreply.github.com>
2022-07-25 23:12:09 +0200

    Add newline between javascript consts

--- bitwarden.py
+++ bitwarden.py
@@ -64,8 +64,9 @@ duplicates = dups_uris(items, func=domains)
 with open("logins.js", "w", encoding='utf-8') as f:
     data = json.dumps(duplicates, sort_keys=True)
     f.write("const logins = JSON.parse(`{}`)".format(re.escape(data)))
-    folders = json.dumps(folders, sort_keys=True)
-    f.write("const folders = JSON.parse(`{}`)".format(re.escape(folders)))
+    f.write("\n\n")
+    folders_str = json.dumps(folders, sort_keys=True)
+    f.write("const folders = JSON.parse(`{}`)".format(re.escape(folders_str)))
 
 # %% Open in visual html file
 webbrowser.open("index.html")
