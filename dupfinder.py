99b5489fb5143876407b7381b8dd0f74eea6157c
Elias <elias@floreteng.se>
2022-08-02 01:12:45 +0200

    Cleanup of code and markup

--- dupfinder.py
+++ dupfinder.py
@@ -42,16 +42,16 @@ def identical_duplicates(logins):
     return dupes
 
 
-def itemrepr(entry, keys=("name", "username", "password", "fields")):
-    """Simple representation of login item"""
-    rep = {k: v for k, v in entry.items() if k in keys}
-    return rep
-
-
 def dups_uris(items, func):
     """Duplicates by set and find"""
     uris = set()
     for item in items:
+        if item.get("fields", None):
+            item["fields"] = [
+                field
+                for field in item["fields"]
+                if field.get("name", None) and field.get("value", None)
+            ]
         item["domains"] = []
         for uri in func(item):
             item["domains"].append(uri)
@@ -63,32 +63,3 @@ def dups_uris(items, func):
         if len(filtered) > 1:
             duplicates[uri] = filtered
     return duplicates
-
-
-def dups_sort(items, func):
-    """Duplicates by sorting and groupby
-    ! DEPRECATED: NOT WORKING
-    """
-    items.sort(key=func)
-    duplicates = [(k, [itemrepr(e) for e in g]) for k, g in groupby(items, key=func)]
-    duplicates = [(dom, ents) for dom, ents in duplicates if len(ents) > 1]
-    return duplicates
-
-
-def dups_seen(items, func):
-    """Duplicates by seen
-    ! DEPRECATED: NOT WORKING
-    """
-    seen = set()
-    for item in items:
-        uris = func(item)
-        if any(uri in seen for uri in uris):
-            print("Duplicate:")
-            yield {
-                k: v
-                for k, v in item.items()
-                if k in ("name", "username", "password", "fields")
-            }
-            print()
-        for uri in uris:
-            seen.add(uri)
